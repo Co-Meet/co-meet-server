7ed96e297f5e6070c53219e0b22906bd4f791eb6
hocaron <pooh0407@ewhain.net>
2022-05-10 19:26:33 +0900

    feat : commit entity 생성

---
+++ src/main/java/com/comeet/commit/Commit.java
@@ -0,0 +1,23 @@
+package com.comeet.commit;
+
+import com.comeet.common.entities.BaseEntity;
+import com.comeet.member.Member;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Commit extends BaseEntity {
+
+    private int count;
+
+    /* 연관관계 */
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
