389b8814b96cf0764469e036dfa6b7f4774f822e
hocaron <pooh0407@ewhain.net>
2022-05-11 18:10:54 +0900

    chore : 프로젝트 이름 변경

--- src/main/java/com/comeet/CoMeetApplication.java
+++ src/main/java/com/comeet/CoMeetApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class CoMeetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CoMeetApplication.class, args);
+    }
 }
