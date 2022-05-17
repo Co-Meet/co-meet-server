3ddfef31731de22853a63086dd5380cb74617a08
hocaron <pooh0407@ewhain.net>
2022-05-17 13:29:14 +0900

    feat : organization 관련 exception 생성

---
+++ src/main/java/com/comeet/organization/exception/OrganizationNotFoundException.java
@@ -0,0 +1,12 @@
+package com.comeet.organization.exception;
+
+
+import com.comeet.common.ResultCode;
+import com.comeet.common.exceptions.NotFoundException;
+
+public class OrganizationNotFoundException extends NotFoundException {
+
+    public OrganizationNotFoundException() {
+        super(ResultCode.ORGANIZATION_NOT_FOUND);
+    }
+}
