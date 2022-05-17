fb61cb5a17704193bdc059a3923e4a4eed2a1d89
hocaron <pooh0407@ewhain.net>
2022-05-17 13:29:46 +0900

    chore : 사용하지 않는 모듈 제거

--- src/main/java/com/comeet/member/entity/Member.java
+++ src/main/java/com/comeet/member/entity/Member.java
@@ -1,7 +1,7 @@
 package com.comeet.member.entity;
 
 import com.comeet.common.entities.BaseEntity;
-import com.comeet.organization.Organization;
+import com.comeet.organization.entity.Organization;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
@@ -30,5 +30,16 @@ public class Member extends BaseEntity {
         member.nickname = nickname;
         member.githubId = githubId;
         return member;
+    }
+
+    public void addOrganization(Organization organization) {
+        if (this.organizations.contains(organization)) {
+            /**
+             * TODO 이미 있는 그룹이라고 에러 핸들링 할지 고민
+             */
+            return;
+        }
+        this.organizations.add(organization);
+        organization.getMembers().add(this);
     }
 }
