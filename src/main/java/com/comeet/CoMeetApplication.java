a0499dd8f816f1ba81023105ee3b95105c676b71
hocaron <pooh0407@ewhain.net>
2022-05-31 19:33:36 +0900

    feat : spring feign 관련 로직 생성

--- src/main/java/com/comeet/CoMeetApplication.java
+++ src/main/java/com/comeet/CoMeetApplication.java
@@ -2,8 +2,10 @@ package com.comeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing
 public class CoMeetApplication {
