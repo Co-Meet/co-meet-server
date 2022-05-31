1b750dfc1def78731c68484af92c185a215293c2
hocaron <pooh0407@ewhain.net>
2022-05-31 17:24:22 +0900

    refactor : SecurityContextHolder 접근하는 유틸클래스 생성

---
+++ src/main/java/com/comeet/config/security/SecurityUtil.java
@@ -0,0 +1,25 @@
+package com.comeet.config.security;
+
+import java.security.Principal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+@Slf4j
+public class SecurityUtil {
+
+    private SecurityUtil() {
+    }
+
+    public static Long resolveMemberId() {
+        final Authentication authentication = SecurityContextHolder.getContext()
+            .getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw new RuntimeException("Security Context 에 인증 정보가 없습니다.");
+        }
+
+        return (Long) authentication.getPrincipal();
+    }
+}
