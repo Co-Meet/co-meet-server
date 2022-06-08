766c20a5361e05a320cbecc2ca77e7b2011b541b
hocaron <pooh0407@ewhain.net>
2022-06-09 01:53:35 +0900

    feat : contribution 파싱 로직 추가

---
+++ src/main/java/com/comeet/commit/model/response/GetCommitResponseDto.java
@@ -0,0 +1,12 @@
+package com.comeet.commit.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GetCommitResponseDto {
+
+    Integer commits;
+
+}
