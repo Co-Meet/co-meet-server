780b224a2bd83d617c863196ef9a0392e1ad562a
hocaron <kkannu0407@gmail.com>
2022-06-15 01:44:07 +0900

    fix : rest docs 생성시 토큰이 필요한 api 요청할 때 필요한 테스트 토큰 코드 생성

--- src/test/java/com/comeet/config/ApiDocumentUtil.java
+++ src/test/java/com/comeet/config/ApiDocumentUtil.java
@@ -12,14 +12,14 @@ public interface ApiDocumentUtil {
 
     static OperationRequestPreprocessor getDocumentRequest() {
         return preprocessRequest(
-            modifyUris()
+            modifyUris() // (1)
                 .scheme("https")
                 .host("docs.api.com")
                 .removePort(),
-            prettyPrint());
+            prettyPrint()); // (2)
     }
 
     static OperationResponsePreprocessor getDocumentResponse() {
-        return preprocessResponse(prettyPrint());
+        return preprocessResponse(prettyPrint()); // (3)
     }
 }
